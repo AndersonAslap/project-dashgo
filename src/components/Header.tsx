6d55327957d45277ae3643304c8b29e6c74fc0cc
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-05-14 02:02:08 -0300

    instalando dependencias e fazendo configuraçoes gerais

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import { Flex, Input, Icon } from '@chakra-ui/react';
+import { Flex, Input, Icon, HStack, Box, Text, Avatar } from '@chakra-ui/react';
 import { Logo } from './Logo';
-import { RiSearchLine } from 'react-icons/ri';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
     return (
@@ -40,6 +40,33 @@ export function Header() {
                 </Input>
 
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                marginLeft="auto"
+            >
+                <HStack
+                    spacing="8"
+                    marginX="8"
+                    paddingRight="8"
+                    paddingY="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex align="center">
+                    <Box marginRight="4" textAlign="right">
+                        <Text>Anderson Aslap</Text>
+                        <Text color="gray.300" fontSize="small">anderson@email.com</Text>
+                    </Box>
+
+                    <Avatar size="md" name="Anderson Adolfo" src="https://avatars.githubusercontent.com/u/43682613?s=400&u=8b8d038f960148a8396fcfbadc11e00fbee40e7f&v=4" />
+                </Flex>
             </Flex>
 
         </Flex>
